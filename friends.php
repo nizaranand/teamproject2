8c78dcef08638d4fef6fce6d5509597a2dd65f32
Jon <systems@myordan.com>
2012-12-06 01:32:13 -0700

    Video looks better now.

--- friends.php
+++ friends.php
@@ -3,7 +3,7 @@
 ?>
 
 <object width="560" height="315">
-	<param name="movie" value="http://www.youtube.com/v/9JtEXMklLMI&autoplay=1?version=3&amp;hl=en_US&amp;rel=0"></param>
+	<param name="movie" value="http://www.youtube.com/v/Qm1osmEK3f8&autoplay=1?version=3&amp;hl=en_US&amp;rel=0"></param>
 	<param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param>
-	<embed src="http://www.youtube.com/v/9JtEXMklLMI&autoplay=1?version=3&amp;hl=en_US&amp;rel=0" type="application/x-shockwave-flash" width="560" height="315" allowscriptaccess="always" allowfullscreen="true"></embed>
+	<embed src="http://www.youtube.com/v/Qm1osmEK3f8&autoplay=1?version=3&amp;hl=en_US&amp;rel=0" type="application/x-shockwave-flash" width="1020" height="630" allowscriptaccess="always" allowfullscreen="false"></embed>
 </object>
